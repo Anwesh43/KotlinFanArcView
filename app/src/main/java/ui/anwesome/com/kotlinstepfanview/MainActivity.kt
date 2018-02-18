1bf6805a8fe904cd59e3c66dc3f4015e261673f4
Anwesh43 <anweshthecool0@gmail.com>
2018-02-19 02:56:01 +0530

    adding fullScreen support

--- app/src/main/java/ui/anwesome/com/kotlinstepfanview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinstepfanview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinstepfanview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.fanrotateview.FanRotateView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FanRotateView.create(this,16)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
