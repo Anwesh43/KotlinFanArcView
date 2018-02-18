18c1532098e64f1aed6dc6f77b9044ace85fe303
Anwesh43 <anweshthecool0@gmail.com>
2018-02-19 02:54:13 +0530

    passing n

--- app/src/main/java/ui/anwesome/com/kotlinstepfanview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinstepfanview/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FanRotateView.create(this)
+        FanRotateView.create(this,16)
     }
 }
